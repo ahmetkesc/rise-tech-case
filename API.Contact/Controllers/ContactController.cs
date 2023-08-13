ec23bc0cd960411c53ac83a62c52598f7a2df293
ahmetkesc <ahmet.kesici@mizanyazilim.com>
2023-08-13 16:37:18 +0300

    feat(restfull methods created): :sparkles: Contact restfull method created
    Contact api restful methods is created.

--- API.Contact/Controllers/ContactController.cs
+++ API.Contact/Controllers/ContactController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Manager;
+using Microsoft.AspNetCore.Mvc;
+using Model.Classes;
 
 namespace API.Contact.Controllers;
 
@@ -6,9 +8,50 @@ namespace API.Contact.Controllers;
 [ApiController]
 public class ContactController : ControllerBase
 {
-    [HttpGet("get")]
-    public IActionResult Get()
+    private IContact _contact;
+
+    public ContactController(IContact contact)
     {
-        return Ok("Contact API running on microservice.");
+        _contact = contact;
+    }
+
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+        var result = _contact.GetAll();
+        if (result.Success) return Ok(result);
+        return BadRequest(result);
+    }
+
+    [HttpGet("getbylocation")]
+    public IActionResult GetByLocation(Guid contactId)
+    {
+        var result = _contact.GetByLocation(contactId);
+        if (result.Success) return Ok(result);
+        return BadRequest(result);
+    }
+
+    [HttpPost("add")]
+    public IActionResult Add(Model.Classes.Contact contact)
+    {
+        var result = _contact.Add(contact);
+        if (result.Success) return Ok(result);
+        return BadRequest(result);
+    }
+
+    [HttpPost("addlocation")]
+    public IActionResult AddLocation(Guid contactId, Location location)
+    {
+        var result = _contact.AddLocation(contactId, location);
+        if (result.Success) return Ok(result);
+        return BadRequest(result);
+    }
+
+    [HttpDelete("deletelocation")]
+    public IActionResult AddLocation(Guid contactId, Guid locationId)
+    {
+        var result = _contact.DeleteLocation(contactId, locationId);
+        if (result.Success) return Ok(result);
+        return BadRequest(result);
     }
 }
