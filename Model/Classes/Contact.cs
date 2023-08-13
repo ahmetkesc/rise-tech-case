6a772e6a815719b1c2608bc7802b4b1ff2f1f8bc
ahmetkesc <ahmet.kesici@mizanyazilim.com>
2023-08-13 15:03:59 +0300

    feat(Model library is generated.): :sparkles: This layer is general classes in project.
    Result, Database Tables Model and DTO.

---
+++ Model/Classes/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+
+namespace Model.Classes;
+
+public class Contact
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Company { get; set; }
+    public string LocationDetail { get; set; }
+
+    [NotMapped] public List<Location>? Locations => JsonConvert.DeserializeObject<List<Location>>(LocationDetail);
+}
+
+public class Location
+{
+    public Guid Id { get; set; }
+    public byte InformationType { get; set; }
+    public string Information { get; set; }
+
+    [JsonIgnore] public Contact? Contact { get; set; }
+}
+
+public class InformationByLocation
+{
+    public long Latitude { get; set; }
+    public long Longitude { get; set; }
+}
---
+++ Model/Classes/Report.cs
@@ -0,0 +1,17 @@
+﻿namespace Model.Classes;
+
+public class Report
+{
+    public Guid Id { get; set; }
+    public DateTime RequestDate { get; set; }
+    public bool IsReady { get; set; }
+    public string ReportSituation { get; set; }
+    public string Detail { get; set; }
+}
+
+public class ReportDetail
+{
+    public string Location { get; set; }
+    public int ContactInLocationCount{ get; set; }
+    public int ContactInLocationByPhoneCount{ get; set; }
+}
